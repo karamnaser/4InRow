e52203b4f03ca5944c0877d6b04d886ee0857653
karamnaser <karam_m_n@hotmail.com>
2019-12-25 14:25:25 +0200

    4inrow

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Games from './game/game';
+
+function App() {
+  return (
+      <div>
+        <Games/>
+   </div>
+  );
+}
+
+export default App;
