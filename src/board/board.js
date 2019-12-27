2397babb157e7c375092b895dd973b1abb1719d5
karamnaser <karam_m_n@hotmail.com>
2019-12-27 09:25:43 +0200

    adding check colum and row functions and modefid move player

--- src/board/board.js
+++ src/board/board.js
@@ -12,7 +12,55 @@ class Board {
         this.pointlocation = 0
     }
 
+    checkrow(point,player){
+        let counter=0;
+        console.log(point);
+        let row_number=point.x;//gitting the point row number
+        console.log(row_number);
+        let board_row=this.board[row_number];
+        console.log(board_row)
+        for(let i=0;board_row.length;i++){
+            if(board_row[i]==player.color){
+                counter++
+                console.log("ther was amach :")
+            }
+            else{
+                break;
+            }
+        }
+        console.log("number of mach cells :",counter)
+            if(counter==4){
+                return true;
+            }
+            else{
+                return false;
+            }
+    }
 
+    checkcolumn(point,player){
+        let counter=0;
+        console.log(point);
+        let column_number=point.y;//gitting the point column number
+        console.log(column_number);
+        for (let i = this.row- 1; i >= 0; i--){
+            if(this.board[i][column_number]==player.color){
+                counter++
+                console.log("ther was amach :")
+            }
+            else{
+                break;
+            }
+        }
+        console.log("number of mach cells :",counter)
+            if(counter==4){
+                return true;
+            }
+            else{
+                return false;
+            }
+    }
+
+    
     creatbored() {
         this.row = prompt('pls enetr number of row (min 4 rows)')
         this.row = this.row < 4 ? 4 : this.row;
@@ -27,6 +75,7 @@ class Board {
             board.push(div_arr)
         }
         this.board = board;
+        console.log(this.board)
         return board;
 
     }
@@ -36,14 +85,15 @@ class Board {
         return this.counter == (this.col * this.row);
     }
 
+
     moveplayer(e, player) {
         let column = e.currentTarget
-        let board_coulumn = this.board[column.getattrebute("column_number")];
-        for (let i = board_coulumn.length - 1; i >= 0; i--) {
-            if (board_coulumn[i] != player.icon && board_coulumn[i] == 0) {
-                this.board[i] = player.icon;
-                this.pointlocation = { x: i, y: column.column_number }
-                console.log(this.pointlocation)
+        let board_coulumn = column.getAttribute("column_number");
+        for (let i = this.row- 1; i >= 0; i--) {
+            if (this.board[i][board_coulumn] != player.icon && this.board[i][board_coulumn] == 0) {
+                this.board[i][board_coulumn] = player.color;
+                console.log(this.board)
+                this.pointlocation = { x: i, y:parseInt(column.getAttribute("column_number"))}
                 return true
             }
 
